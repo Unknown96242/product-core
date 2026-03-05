4ed66b42e84689bde102d63383f95d1f1706f4a2
Unknown96242 <raytchikaya6.5@icloud.com>
2026-03-05 10:14:07 +0000

    feat: add createProduct() - version 0.0.1-SNAPSHOT

---
+++ src/main/java/sn/isi/l3gl/core/service/ProductService.java
@@ -0,0 +1,17 @@
+package sn.isi.l3gl.core.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sn.isi.l3gl.core.entity.Product;
+import sn.isi.l3gl.core.repository.ProductRepository;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+}
